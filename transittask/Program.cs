d0ef306d8703a2721c46f79fa57a14e919d1081a
Vera Lippgardt <vera1078@mail.ru>
2022-09-07 21:17:52 +0300

    Переход от одной задачи к другой

---
+++ transittask/Program.cs
@@ -0,0 +1,26 @@
+﻿// Для перехода от одной задачи к другой, переходы по именам методов
+
+Console.WriteLine("\n Введите номер задачи (34, 36, 37 или 38): \t");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine();
+
+if (a == 34)
+{
+    RandomArray();
+}
+
+else if (a == 36)
+{
+    SumOfElements();
+}
+
+else if (a == 37)
+{
+    ResultArray();
+}
+
+else if (a == 38)
+{
+     RealNambersArray();
+}
+
